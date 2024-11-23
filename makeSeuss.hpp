124c8de5de93f2ebab1f82e357bb996c1bb74b5b
Ronith Anchan <ronith.anchan@yahoo.com>
2024-11-23 18:09:36 -0500

    Update makeSeuss.hpp

--- makeSeuss.hpp
+++ makeSeuss.hpp
@@ -6,8 +6,8 @@ using namespace std;
 
 class makeSeuss {
     hashMap *ht;
-    string fn; // file name for input (“DrSeuss.txt”)
-    string newfile; // name of output file
+    string fn; 
+    string newfile; 
 public:
     makeSeuss(string file,string newf,bool h1, bool c1);
     void readFile();
